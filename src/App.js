b09a411f383df3b4f91fa8fd6ab0bdbcb7c1c29b
Rana Muzamal <muzamilrana149@gmail.com>
2022-08-21 06:11:18 -0700

    Passwrod Hider which is mostly used in website form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [username,setUsername]=useState('');
+  const [password,setPassword]=useState('');
+  const [showPassword,setShowPassword]=useState(true)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+      
+       <div className="container">
+        <h2 className='title'>PASSWORD HIDER</h2>
+        <input 
+          type='text'
+          placeholder='Enter Username'
+          value={username} 
+          onChange={(e)=>setUsername(e.target.value)}
+        />
+        <input 
+          type={showPassword ? 'text':'password'}
+          placeholder='Énter Password'
+          value={password}
+          onChange={(e)=> setPassword(e.target.value)}
+        />
+        <h2>{username}</h2>
+        <h2>{showPassword ? password:"*".repeat(password.length)}</h2>
+        <button onClick={(e)=>setShowPassword(!showPassword)}>Hide/Show Password</button>
+ 
+
     </div>
+   
   );
 }
 
